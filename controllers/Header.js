e6cf49dd500a77b1d76a51a6528f8463300c9bd6
Peter <47840148+Sweedsiter@users.noreply.github.com>
2024-01-04 10:01:05 +0000

    Update 6 files

--- controllers/Header.js
+++ controllers/Header.js
@@ -1,16 +1,22 @@
 const fs = require("fs");
 
+// map file controller
+
 exports.addhaeder = async (req, res) => {
   const heaterContact = req.body.heaterContact;
+  const links = req.body.links;
   await fs.writeFile(
-    `./views/heater.ejs`,
-    `<h1>${heaterContact}</h1>`,
+    `./views/header.ejs`,
+    `<div>
+        <div class="logo"><h1>${heaterContact}</h1></div>
+        <div class="links"><h1>${links}</h1></div>
+    </div>`,
     function (err) {
       if (err) throw err;
       console.log(`Add header ${heaterContact} เรียบร้อย`);
     },
   );
-  await fs.readFile("./views/heater.ejs", "utf-8", function (rer, data) {
+  await fs.readFile("./views/header.ejs", "utf-8", function (rer, data) {
     if (rer) {
       console.log("เกิดผิตพลาด", rer);
     } else {
--- controllers/controller.js
+++ controllers/controller.js
@@ -8,17 +8,3 @@ exports.home = async (req, res) => {
   // console.log(process.env.WEATER_API_KEY);
   // console.log(process.env.COLOR);
 };
-
-exports.heatEdit = async (req, res) => {
-  await fs.writeFile("./public/heater.txt", "Hello content", function (err) {
-    if (err) throw err;
-    console.log("Saved!");
-  });
-  await fs.readFile("./public/heater.txt", "utf-8", function (rer, data) {
-    if (rer) {
-      console.log("เกิดผิตพลาด", rer);
-    } else {
-      res.send(data);
-    }
-  });
-};
