0cd27aa9e385d1dd8aab5f2aea3e1ba33cb8a6ab
Peter <47840148+Sweedsiter@users.noreply.github.com>
2024-01-17 07:38:52 +0000

    Update 15 files

--- Routes/router.js
+++ Routes/router.js
@@ -5,6 +5,8 @@ const { login, logut } = require("../controllers/Login-out/Login_out"); // Log i
 const { H001 } = require("../controllers/Home/HomePage001");
 const { home, PageColor } = require("../controllers/controller");
 const multer = require("multer");
+const { H002Promo01 } = require("../controllers/Home/HomePage002");
+
 // Logo Upload
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -23,34 +25,53 @@ const storageH001 = multer.diskStorage({
     cb(null, "./public/avatar/Home");
   },
   filename: function (req, file, callback) {
-    callback(null, Date.now() + ".jpg");
+    callback(null,"H001BG"+".jpg");
   },
 });
 const uploadH001BG = multer({
   storage: storageH001,
 });
 
-router.get("/", home);
-router.post("/HomeEdit001", uploadH001BG.single("H001BG"), H001);
-router.post("/HomeEdit002", (req, res) => {
-  const t = req.body;
-  res.render("Dashboard");
-  console.log(t);
+// H002 Upload img promo01
+const storageH002 = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/avatar/Home/H002/promo01");
+  },
+  filename: function (req, file, callback) {
+    callback(null, "H002promo01" + ".jpg");
+  },
+});
+const uploadH002 = multer({
+  storage: storageH002,
 });
 
+// Home page all details
+router.get("/", home);
+router.post("/addheader", uploadlogo.single("logoimg"), addhaeder); // การแก้ Header
+router.post("/HomeEdit001", uploadH001BG.single("H001BG"), H001);
+router.post(
+  "/HomeEdit002/promo01",
+  uploadH002.single("promo01Img"),
+  H002Promo01,
+); // การแก้ H002-promo01
+
+// Products All show
 router.get("/product", (req, res) => {
   res.render("Product");
 });
+
+// About All show
 router.get("/about", (req, res) => {
   res.render("About");
 });
+
+// Contact All show
 router.get("/contact", (req, res) => {
   res.render("Contact");
 });
 
 // post
 
-router.post("/addheader", uploadlogo.single("logoimg"), addhaeder); // การแก้
 router.post("/PageColor", PageColor); // การแก้
 
 router.post("/login", login);
--- controllers/Home/HomePage002.js
+++ controllers/Home/HomePage002.js
@@ -1,4 +1,15 @@
-exports.H002 = async (req, res) => {
-  await res.redirect("/dashboard");
-  console.log("H002 Edit Ok", req.body);
+// Homepage 02 promotion 01
+exports.H002Promo01 = async (req, res) => {
+  await require("fs").writeFile(
+    "./public/avatar/Home/H002/promo01/promo01.txt",
+    req.body.promo01Head,
+    function () {},
+  );
+  await require("fs").writeFile(
+    "./public/avatar/Home/H002/promo01/promo01Img.txt",
+    req.file.filename,
+    function () {},
+  );
+  await res.render("Dashboard");
+  console.log("HomeEdit Two ");
 };
